175f82252ac65e3dc5445476784ad0a63ba6f508
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 18:12:12 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,13 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = 
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  puts ""
